ca3c955fe2ec6342d4517e173b7e9c0301bd80f3
samarsrivastav <samarsrivastav69@gmail.com>
2025-03-21 12:02:52 +0530

    cors fix

--- KDU/src/main/java/backend/config/CorsConfig.java
+++ KDU/src/main/java/backend/config/CorsConfig.java
@@ -23,7 +23,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         logger.info("Applying CORS settings...");
         registry.addMapping("/api/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
