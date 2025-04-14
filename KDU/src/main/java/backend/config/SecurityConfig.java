84ded791a6d00167ad2e9909fe497cc60fcc7a9a
manan gupta <you@example.com>
2025-04-14 19:54:53 +0530

    cors change 4

--- KDU/src/main/java/backend/config/SecurityConfig.java
+++ KDU/src/main/java/backend/config/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig {
         log.info("Using issuer URI: {}", issuerUri);
 
         http
-                .cors(cors -> cors.disable())
+                .cors()
+                .and()
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
                     log.info("Configuring authorization rules...");
