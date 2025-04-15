d0aaa747582db43b517c98824c177e8fe6191cbe
samarsrivastav <samarsrivastav69@gmail.com>
2025-04-15 13:50:24 +0530

    Smtp config

--- Terraform/modules/email_template_system/lambda/index.js
+++ Terraform/modules/email_template_system/lambda/index.js
@@ -18,7 +18,7 @@ const dbConfig = {
 const smtpConfig = {
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: 'false',
+    secure: false,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD
